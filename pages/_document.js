f5942d3a2f47c49e73eb75cd0011f196cb742455
Steven Widharma <steven.widharma@gmail.com>
2023-01-15 21:40:10 +0800

    Clean up demo

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
-          <link rel="shortcut icon" href="/demo/img/brand/favicon.ico" />
+          <link rel="shortcut icon" href="/img/TurboLaserLogo-600px.png" />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
